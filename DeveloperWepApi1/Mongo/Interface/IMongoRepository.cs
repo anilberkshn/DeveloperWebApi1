7802f939dc9bb4186a584305a67e96c7033d73dc
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2022-12-29 16:42:15 +0300

    IMongoRepository.cs

--- DeveloperWepApi1/Mongo/Interface/IMongoRepository.cs
+++ DeveloperWepApi1/Mongo/Interface/IMongoRepository.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using DeveloperWepApi1.Model.Entities;
+using DeveloperWepApi1.Model.RequestModels;
+using MongoDB.Driver;
+
 namespace DeveloperWepApi1.Mongo.Interface
 {
     public interface IMongoRepository<T>
     {
-        
+        public Guid Create (T record);
+        public List<T> FindAll();
+        public T FindOne(Expression<Func<T, bool>> expression);
+
+        public void Update(Expression<Func<T, bool>> expression, UpdateDefinition<T> updateDefinition);
+    
     }
 }
