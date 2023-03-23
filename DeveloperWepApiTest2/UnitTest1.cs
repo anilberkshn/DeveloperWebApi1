44ef460cf050545dc95b5f48d09768ca471eca7f
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-03-23 18:46:11 +0300

    UnitTest Crud işlemleri ile ilgili mock ve nunit yapma çalışmaları.

--- DeveloperWepApiTest2/UnitTest1.cs
+++ DeveloperWepApiTest2/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using DeveloperWepApi1.DeveloperRepository;
+using DeveloperWepApi1.Model.Entities;
 using DeveloperWepApi1.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -9,29 +10,76 @@ using Assert = NUnit.Framework.Assert;
 namespace DeveloperWepApiTest2
 {
     [TestFixture]
-    public class Tests
+    public class DeveloperServiceUnitTest
     {
-        private Mock<DeveloperRepository> _mock;
-        
-        
-        [TestInitialize]
-        public void StartMoq()
+        private Mock<DeveloperService> _mockDeveloperService;
+        private Mock<DeveloperRepository> _mockDeveloperRepository;
+  
+        [OneTimeSetUp]
+        public void Setup()
         {
-            _mock = new Mock<DeveloperRepository>();
+            _mockDeveloperRepository = new Mock<DeveloperRepository>();
+            _mockDeveloperService = new Mock<DeveloperService>();
         }
         
-        
-        
-        
-        [TestMethod]
-        public void Test1()
+        [Test]
+        public void GetById_developerIsNullCheck_Test1()
         {
             //Arrange
-            var developer = new DeveloperService(_mock.Object);
+            _mockDeveloperRepository.Setup(x => 
+                    x.GetById(It.IsAny<Guid>()))
+                .Returns(new Developer
+                {
+                    Id = default,
+                    CreatedTime = default,
+                    UpdatedTime = default,
+                    DeleteTime = default,
+                    IsDeleted = false,
+                    Name = null,
+                    Surname = null,
+                    Department = null,
+                    Username = null,
+                    Password = null,
+                    RefreshToken = null,
+                    RefreshTokenEndDate = null
+                });
+                
+                
+                
+            var developer = new DeveloperService(_mockDeveloperRepository.Object);
             //Act
-            var dev = developer.GetById(Guid.Parse("f9a08115-776f-49f4-b267-36f7ce0d126a"));
+            //var dev = developer.GetById(Guid.Parse("f9a08115-776f-49f4-b267-36f7ce0d126a"));
+            var dev = developer.GetById(default);
             //Assert
             Assert.NotNull(dev);
+        }
+        
+        [Test]
+        public void GetAll_Developer_Developer_Test1()
+        {
+            //arrange
+            //act
+            //assert
+        } 
+        [Test]
+        public void Delete_Developer_Developer_Test1()
+        {
+            //arrange
+            //act
+            //assert
+        }  [Test]
+        public void SoftDelete_Developer_Developer_Test1()
+        {
+            //arrange
+            //act
+            //assert
+        } 
+        [Test]
+        public void Put_Developer_Developer_Test1()
+        {
+            //arrange
+            //act
+            //assert
         }
     }
 }
